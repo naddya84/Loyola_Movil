38d0b03f04057a9f380de94070399049476a5ace
Pablo <Pablof3@outlook.com>
2021-12-01 11:48:23 -0400

    toast notice of no internet connection in pdf download

--- app/src/main/java/app/wiserkronox/loyolasocios/service/repository/CertificateRest.kt
+++ app/src/main/java/app/wiserkronox/loyolasocios/service/repository/CertificateRest.kt
@@ -7,7 +7,10 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -82,63 +85,98 @@ class CertificateRest(val context: Activity){
     }
 
     fun getListCertificatesToPdf(docuCage: String ="") {
-        var dowloadid:Long = 0
-        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(context as Activity, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
-        } else {
-            val new = object: BroadcastReceiver() {
-                override fun onReceive(p0: Context?, p1: Intent?) {
-                    val id = p1?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-                    if (id == dowloadid){
-                        Toast.makeText(context, "Descarga completada", Toast.LENGTH_SHORT).show()
+        if (isNetworkAvailable(context)) {
+            var dowloadid:Long = 0
+            if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(context as Activity, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+            } else {
+                val new = object: BroadcastReceiver() {
+                    override fun onReceive(p0: Context?, p1: Intent?) {
+                        val id = p1?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+                        if (id == dowloadid){
+                            Toast.makeText(context, "Descarga completada", Toast.LENGTH_SHORT).show()
+                        }
                     }
+
                 }
 
+                val uri ="${context.resources.getString(R.string.host_service)}${context.getString(R.string.home_service)}$GET_CERTIFICATES_PDF?docu_cage=${docuCage}"
+                val request = DownloadManager.Request(Uri.parse(uri))
+                    .setTitle("certificados-loyola.pdf")
+                    .setDescription("Descargando....")
+                    .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "certificados-loyola.pdf")
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                    .setAllowedOverMetered(true)
+
+                val dowloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                dowloadid = dowloadManager.enqueue(request)
+
+                context.registerReceiver(new, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
             }
-
-            val uri ="${context.resources.getString(R.string.host_service)}${context.getString(R.string.home_service)}$GET_CERTIFICATES_PDF?docu_cage=${docuCage}"
-            val request = DownloadManager.Request(Uri.parse(uri))
-                .setTitle("certificados-loyola.pdf")
-                .setDescription("Descargando....")
-                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "certificados-loyola.pdf")
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                .setAllowedOverMetered(true)
-
-            val dowloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-            dowloadid = dowloadManager.enqueue(request)
-
-            context.registerReceiver(new, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
         }
+        else
+            Toast.makeText(context, "Error de conexión con el servidor", Toast.LENGTH_SHORT).show()
     }
 
     fun getCertificatePdf(certificateNumber: String ="") {
-        var dowloadid:Long = 0
-        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(context as Activity, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
-        } else {
-            val new = object: BroadcastReceiver() {
-                override fun onReceive(p0: Context?, p1: Intent?) {
-                    val id = p1?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-                    if (id == dowloadid){
-                        Toast.makeText(context, "Descarga completada", Toast.LENGTH_SHORT).show()
+        if (isNetworkAvailable(context)) {
+            var dowloadid:Long = 0
+            if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(context as Activity, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+            } else {
+                val new = object: BroadcastReceiver() {
+                    override fun onReceive(p0: Context?, p1: Intent?) {
+                        val id = p1?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+                        if (id == dowloadid){
+                            Toast.makeText(context, "Descarga completada", Toast.LENGTH_SHORT).show()
+                        }
                     }
+
                 }
 
+                val uri ="${context.resources.getString(R.string.host_service)}${context.getString(R.string.home_service)}$GET_CERTIFICATE_PDF?certificate_number=${certificateNumber}"
+                val request = DownloadManager.Request(Uri.parse(uri))
+                    .setTitle("certificado-$certificateNumber.pdf")
+                    .setDescription("Descargando....")
+                    .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "certificado-$certificateNumber.pdf")
+                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                    .setAllowedOverMetered(true)
+
+                val dowloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                dowloadid = dowloadManager.enqueue(request)
+
+                context.registerReceiver(new, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
             }
-
-            val uri ="${context.resources.getString(R.string.host_service)}${context.getString(R.string.home_service)}$GET_CERTIFICATE_PDF?certificate_number=${certificateNumber}"
-            val request = DownloadManager.Request(Uri.parse(uri))
-                .setTitle("certificado-$certificateNumber.pdf")
-                .setDescription("Descargando....")
-                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "certificado-$certificateNumber.pdf")
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                .setAllowedOverMetered(true)
-
-            val dowloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-            dowloadid = dowloadManager.enqueue(request)
-
-            context.registerReceiver(new, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
         }
+        else
+            Toast.makeText(context, "Error de conexión con el servidor", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isNetworkAvailable(context: Context?): Boolean {
+        if (context == null) return false
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
     }
 
 }
