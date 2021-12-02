4971f5f87860e210b447cd03f294d91f60878401
DkChriss <chrisshuayhua@hotmail.com>
2021-12-02 14:27:17 -0400

    Add class for database local in local repository

--- app/src/main/java/app/wiserkronox/loyolasocios/service/repository/LoyolaRepository.kt
+++ app/src/main/java/app/wiserkronox/loyolasocios/service/repository/LoyolaRepository.kt
@@ -8,7 +8,12 @@ class LoyolaRepository(
     private val userDao: UserDao,
     private val assemblyDao: AssemblyDao,
     private val courseDao: CourseDao,
-    private val certificateDao: CertificateDao
+    private val certificateDao: CertificateDao,
+    private val creditDao: CreditDao,
+    private val creditPlanPayDao: CreditPlanPayDao,
+    private val creditPlanPayDetailDao: CreditPlanPayDetailDao,
+    private val creditExtractDao: CreditExtractDao,
+    private val creditExtractDetailDao: CreditExtractDetailDao
 ) {
 
     val allUsers: Flow<List<User>> = userDao.getUsers()
@@ -136,5 +141,104 @@ class LoyolaRepository(
     @WorkerThread
     suspend fun deleteAllCertificates(){
         certificateDao.deleteAll()
+    }
+
+    /*************************************************************************************
+     * Funciones para la instancia de creditos
+     */
+    @Suppress("RedundantSuspendModifier")
+    suspend fun getAllCredits():List<Credit> {
+        return  creditDao.getAllCredits()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insertAllCredits(credits: List<Credit>): List<Long>{
+        return creditDao.insertAllCredits(credits)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteAllCredits(){
+        creditDao.deleteAllCredits()
+    }
+
+    /*************************************************************************************
+     * Funciones para la instancia de creditos plan de pagos
+     */
+    @Suppress("RedundantSuspendModifier")
+    suspend fun getAllCreditsPlanPay():List<CreditPlanPay> {
+        return  creditPlanPayDao.getAllCreditsPlanPay()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insertAllCreditsPlanPay(credtisplanpay: List<CreditPlanPay>): List<Long>{
+        return creditPlanPayDao.insertAllCreditsPlanPay(credtisplanpay)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteAllCreditsPlanPay(){
+        creditPlanPayDao.deleteAllCreditsPlanPay()
+    }
+
+    /*************************************************************************************
+     * Funciones para la instancia de creditos plan de pagos detalle
+     */
+    @Suppress("RedundantSuspendModifier")
+    suspend fun getAllCreditsPlanPayDetail():List<CreditPlanPayDetail> {
+        return  creditPlanPayDetailDao.getAllCreditsPlanPayDetail()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insertAllCreditsPlanPayDetail(creditsplanpaydetail: List<CreditPlanPayDetail>): List<Long>{
+        return creditPlanPayDetailDao.insertAllCreditsPlanPayDetail(creditsplanpaydetail)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteAllCreditsPlanPayDetail(){
+        creditPlanPayDetailDao.deleteAllCreditsPlanPayDetail()
+    }
+
+    /*************************************************************************************
+     * Funciones para la instancia de creditos extracto
+     */
+    @Suppress("RedundantSuspendModifier")
+    suspend fun getAllCreditsExtract():List<CreditExtract> {
+        return  creditExtractDao.getAllCreditsExtract()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insertAllCreditsExtract(creditsextract: List<CreditExtract>): List<Long>{
+        return creditExtractDao.insertAllCreditsExtract(creditsextract)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteAllCreditsExtract(){
+        creditExtractDao.deleteAllCreditsExtract()
+    }
+    /*************************************************************************************
+     * Funciones para la instancia de creditos extracto detalle
+     */
+    @Suppress("RedundantSuspendModifier")
+    suspend fun getAllCreditsExtractDetail():List<CreditExtractDetail> {
+        return  creditExtractDetailDao.getAllCreditsExtractDetail()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insertAllCreditsExtractDetail(creditsextractdetail: List<CreditExtractDetail>): List<Long>{
+        return creditExtractDetailDao.insertAllCreditsExtractsDetail(creditsextractdetail)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun deleteAllCreditsExtractDetail(){
+        creditExtractDetailDao.deleteAllCreditsExtractDetail()
     }
 }
